e670cc017969dcf114017b01705eab03f4d4f56c
Yazan Khawaldeh <yazank.19@gmail.com>
2026-07-04 03:40:18 -0700

    Definition and implementation of structs and functions, as well as enums

--- perkdb.c
+++ perkdb.c
@@ -1,5 +1,66 @@
 #include "perkdb.h"
+#include "sqlite3-src/sqlite3.h"
+#include <stdio.h>
+#include <stdlib.h>
+struct PerkDB
+{
+    sqlite3 *conn;
+    char errmsg[256];
+    int in_transaction;
+};
+
+struct PerkResult
+{
+    size_t nrows, ncols;
+    char **col_names;
+    PerkValue *cells;
+};
+
+PerkErr perk_open(const char *path, PerkDB **out)
+{
+    if (out == NULL || path == NULL)
+    {
+        return PERK_ERR_OPEN;
+    }
+    *out = NULL;
+
+    PerkDB *db = calloc(1, sizeof(*db));
+    if (db == NULL)
+        return PERK_ERR_NOMEM;
+
+    if (sqlite3_open(path, &db->conn) != SQLITE_OK)
+    {
+        sqlite3_close(db->conn);
+        free(db);
+        return PERK_ERR_OPEN;
+    }
+
+    const char *pragmas = 
+        "PRAGMA journal_mode = WAL;"
+        "PRAGMA busy_timeout = 5000;"
+        "PRAGMA foreign_keys = ON"
+        "PRAGMA synchronous = NORMAL";
+    if (sqlite3_exec(db->conn, pragmas, NULL, NULL, NULL) != SQLITE_OK)
+    {
+        snprintf(db->errmsg, sizeof(db->errmsg), "%s", sqlite3_errmsg(db->conn));
+        sqlite3_close(db->conn);
+        free(db);
+        return PERK_ERR_OPEN;
+    }
+    *out = db;
+    return PERK_OK;
+}
 
 
+void perk_close(PerkDB *db)
+{
+    if (db == NULL)
+    {
+        return;
+    }
+
+    sqlite3_close(db->conn);
+    free(db);
+}
 
 
--- perkdb.h
+++ perkdb.h
@@ -6,15 +6,58 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+
+// This is defined in perkdb.c
 typedef struct PerkDB PerkDB;
+
+
 typedef enum {
     PERK_OK = 0,
     PERK_ERR_OPEN, PERK_ERR_SQL, PERK_ERR_BUSY,
     PERK_ERR_BADNAME, PERK_ERR_NOMEM
 } PerkErr;
 
-typedef struct { const char *name; const char *type; int flags; } PerkColumn;
-typedef struct { } PerkValue;
+// Column inputs to perk_create_table() and flags for Primary Key, Not Null, and Unique
+typedef enum {
+    PERK_COL_PK = 1 << 0,
+    PERK_COL_NOTNULL = 1 << 1,
+    PERK_COL_UNIQUE = 1 << 2,
+} PerkColFlags;
+
+typedef struct {
+    const char *name;
+    const char *type;
+    int flags;
+} PerkColumn;
+
+// Definitions for PerkValue's, generics that are inputted for data insertion into the db
+
+typedef enum {
+    PERK_NULL,
+    PERK_INT,
+    PERK_REAL,
+    PERK_TEXT,
+    PERK_BLOB
+} PerkType;
+
+
+typedef struct {
+    PerkType type;
+    union {
+        int64_t i;
+        double r;
+        struct { const char *ptr; size_t len; } text;
+        struct { const void *ptr; size_t len; } blob;
+    } as;
+} PerkValue;
+
+static inline PerkValue perk_int(int64_t v);
+static inline PerkValue perk_real(double v);
+static inline PerkValue perk_text(const char *v);
+static inline PerkValue perk_blob(const void *p, size_t n);
+static inline PerkValue perk_null(void);
+
+
 typedef struct PerkResult PerkResult;
 
 PerkErr perk_open(const char *path, PerkDB **out);
